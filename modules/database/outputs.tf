d23e0fca1f0fcf2a3ac31416a7181ea95bb09abe
Chjjh605 <chjjh605@gmail.com>
2026-06-05 22:49:16 +0900

    CJH: Outputs 추가

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -2,3 +2,13 @@
 output "sqs_queue_url" {
   value = aws_sqs_queue.queue.url
 }
+
+# Aurora MySQL 클러스터 엔드포인트 주소
+output "aurora_writer_endpoint" {
+  value       = aws_rds_cluster.aurora_cluster.endpoint
+}
+
+# ElastiCache Redis 엔드포인트 주소
+output "redis_primary_endpoint" {
+  value       = aws_elasticache_replication_group.redis.primary_endpoint_address
+}
