346817f974521eb417350715b3c112d03c287655
Ra-jin <hajin0335@gmail.com>
2026-06-10 18:23:09 +0900

    hjin:타겟그룹바인딩 방식 생성

---
+++ modules/networking/alb.tf
@@ -0,0 +1,33 @@
+resource "aws_lb_target_group" "app_tg" {
+  name        = "${var.project_name}-${var.environment}-app-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/healthz"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "routing_rule" {
+  listener_arn = aws_lb_listener.front_http.arn 
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
