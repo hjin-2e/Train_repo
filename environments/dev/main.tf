a132a3869c913baca1fa817fb7f49520350f0816
sum9191-ops <sum9191@gmail.com>
2026-06-12 17:11:37 +0900

    fix: temporary relax oidc sub to * for debugging

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -89,7 +89,7 @@ module "frontend-pipeline" {
   frontend_bucket_name       = module.networking.s3_frontend_bucket
   cloudfront_distribution_id = module.networking.cloudfront_distribution_id
 
-  github_repo = "sum9191-ops/*"
+  github_repo = "*"
 
   create_github_oidc_provider = true
 }
@@ -99,7 +99,7 @@ module "backend-pipeline" {
   project_name = var.project_name
   environment  = var.environment
 
-  github_repo = "Chjjh605/*"
+  github_repo = "*"
 }
 
 # 내부 통신 알림
