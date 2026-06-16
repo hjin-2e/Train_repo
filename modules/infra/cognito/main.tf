20e2aa6bb01c4ef0e270ae5a9679f1d6871c7749
Chjjh605 <chjjh605@gmail.com>
2026-06-16 20:09:20 +0900

    JH: Cognito 연동 에러 수정

--- modules/infra/cognito/main.tf
+++ modules/infra/cognito/main.tf
@@ -2,15 +2,14 @@
 resource "aws_cognito_user_pool" "pool" {
   name = "${var.project_name}-${var.environment}-user-pool"
 
-  username_attributes      = ["email"]
   auto_verified_attributes = ["email"]
 
   password_policy {
-    minimum_length    = 8
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
+    minimum_length    = 4
+    require_lowercase = false
+    require_numbers   = false
+    require_symbols   = false
+    require_uppercase = false
   }
 
   schema {
